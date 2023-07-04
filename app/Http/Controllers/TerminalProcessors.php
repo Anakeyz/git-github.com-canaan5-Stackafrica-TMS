e2bbe5c8959cf14aa77aa987d9c87b760b25a318
Lyte Onyema <lyte.onyema@gmail.com>
2023-07-04 22:13:55 +0100

    Update terminal processor

--- app/Http/Controllers/TerminalProcessors.php
+++ app/Http/Controllers/TerminalProcessors.php
@@ -20,7 +20,11 @@ class TerminalProcessors extends Controller
     public function update(Request $request, TerminalProcessor $terminalProcessor)
     {
         $data = $request->validate([
-
+            'serial' => 'string',
+            'tid' => 'string',
+            'mid' => 'string',
+            'category_code' => 'string|size:4',
+            'name_location' => 'string|size:40'
         ]);
 
         $terminalProcessor->update($data);
