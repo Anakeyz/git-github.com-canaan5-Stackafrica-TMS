a95ae7abf07a2d5556e55b8565d3a0b4bc289366
Lyte Onyema <lyte.onyema@gmail.com>
2023-07-07 09:59:30 +0100

    Show approval resource details in modal

--- resources/views/components/approvals/show-details.blade.php
+++ resources/views/components/approvals/show-details.blade.php
@@ -2,44 +2,74 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <a data-tw-dismiss="modal" href="javascript:;"> <i data-lucide="x" class="w-8 h-8 text-slate-400"></i> </a>
-            <!-- BEGIN: Slide Over Header -->
-            <div class="modal-header">
-                <h2 class="font-medium text-base mr-auto slide-over-title">
-                    Show resource approval details
-                </h2>
+            <template x-if="Object.keys(approval).length !== 0">
+                <div>
+                    <!-- BEGIN: Slide Over Header -->
+                    <div class="modal-header">
+                        <h2 class="text-lg font-medium"
+                            x-text="`${approval.resource} was ${approval.action?.toUpperCase()} by ${approval.author.name}`">
+                        </h2>
 
-            </div>
-            <!-- END: Slide Over Header -->
-            <div class="modal-body">
-
-                <p x-text="console.log(approval)"></p>
-
-            </div>
-            <!-- END: Slide Over Body -->
-            <!-- BEGIN: Slide Over Footer -->
-            <div class="modal-footer w-full flex justify-end gap-4 my-3">
-                <form :action="declineRoute" method="post" class="my-form">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-outline-danger px-5 w-fit spinner-dark">
-                        <div class="flex gap-1">
-                            <i data-lucide="thumbs-down" class="w-4 h-4 mr-1"></i>  Decline
+                    </div>
+                    <!-- END: Slide Over Header -->
+                    <div class="modal-body">
+                        <p>
+                            <span class="font-medium" x-text="`Date ${approval.action}:`"></span>
+                            <span x-text="(new Date(approval.created_at)).toGMTString().replace('GMT', '')"></span>
+                        </p>
+                        <div class="grid grid-cols-3 mt-2 font-medium text-slate-500">
+                            <div class="col-span-1">Details</div>
+                            <div class="col-span-1">New value</div>
+                            <template x-if="approval.action !== 'created'">
+                                <div class="col-span-1">Old value</div>
+                            </template>
                         </div>
-                    </button>
-                </form>
 
-                <form :action="approveRoute" method="post" class="my-form">
-                    @csrf
-                    @method('PUT')
+                        <hr>
 
-                    <button type="submit" class="btn btn-success text-white px-5 w-fit">
-                        <div class="flex gap-1">
-                            <i data-lucide="thumbs-up" class="w-4 h-4 mr-1"></i> Approve
-                        </div>
-                    </button>
-                </form>
-            </div>
-            <!-- END: Slide Over Footer -->
+                        <template x-for="key in Object.keys(approval.new_data)" :key="key">
+                            <div class="grid grid-cols-3 py-1.5">
+                                <div class="col-span-1 font-medium" x-text="key.replace('_', ' ')"></div>
+                                <div class="col-span-1" x-text="approval.new_data[key]"></div>
+                                <template x-if="approval.action !== 'created'">
+                                    <div class="col-span-1" x-text="approval.original_data[key]"></div>
+                                </template>
+                            </div>
+                        </template>
+                    </div>
+                    <!-- END: Slide Over Body -->
+                    <!-- BEGIN: Slide Over Footer -->
+                    <div class="modal-footer w-full flex justify-end gap-4 my-3">
+                        <form :action="approvalRoute" method="post" class="my-form">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-outline-danger px-5 w-fit spinner-dark">
+                                <div class="flex gap-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="thumbs-down" data-lucide="thumbs-down" class="lucide lucide-thumbs-down w-4 h-4 mr-1">
+                                        <path d="M10 15v4a3 3 0 003 3l4-9V2H5.72a2 2 0 00-2 1.7l-1.38 9a2 2 0 002 2.3zm7-13h2.67A2.31 2.31 0 0122 4v7a2.31 2.31 0 01-2.33 2H17"></path>
+                                    </svg>
+                                    Decline
+                                </div>
+                            </button>
+                        </form>
+
+                        <form :action="approvalRoute" method="post" class="my-form">
+                            @csrf
+                            @method('PUT')
+
+                            <button type="submit" class="btn btn-success text-white px-5 w-fit">
+                                <div class="flex gap-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="thumbs-up" data-lucide="thumbs-up" class="lucide lucide-thumbs-up w-4 h-4 mr-1">
+                                        <path d="M14 9V5a3 3 0 00-3-3l-4 9v11h11.28a2 2 0 002-1.7l1.38-9a2 2 0 00-2-2.3zM7 22H4a2 2 0 01-2-2v-7a2 2 0 012-2h3"></path>
+                                    </svg>
+                                    Approve
+                                </div>
+                            </button>
+                        </form>
+                    </div>
+                    <!-- END: Slide Over Footer -->
+                </div>
+            </template>
         </div>
     </div>
 </div>
--- resources/views/livewire/approvals-table.blade.php
+++ resources/views/livewire/approvals-table.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{approval: {}, approveRoute: null, declineRoute: null}">
+<div x-data="{approval: {}, approvalRoute: null}">
 
     <div class="sm:mb-3 flex flex-col-reverse sm:flex-row justify-between sm:items-center mt-5 intro-y">
         <p class="font-semibold">Showing list of all pending approvals for actions</p>
@@ -17,7 +17,7 @@
 
                     <th scope="col" class="text-center">Action</th>
 
-                    <th scope="col">Performed By</th>
+                    <th scope="col" class="whitespace-nowrap">Performed By</th>
 
                     <th scope="col" class="text-center">Date</th>
 
@@ -44,14 +44,14 @@
 
                         <td class="table-report__action w-56">
                             <div class="flex justify-around items-center">
-                                {{--<button class="flex items-center mr-3 text-blue-600"
+                                <button class="flex items-center mr-3 text-blue-600"
                                         data-tw-toggle="modal" data-tw-target="#view-approval"
-                                        @click="approval = @js($approval); approveRoute = '{{ route('approvals.update', $approval) }}'; declineRoute = '{{route('approvals.destroy', $approval) }}'"
+                                        @click="approvalRoute = '{{ route('approvals.update', $approval) }}'; approval = @js($approval)"
                                 >
-                                    <i data-lucide="eye" class="w-4 h-4 mr-1"></i> View
-                                </button>--}}
+                                    <i data-lucide="edit" class="w-4 h-4 mr-1"></i> Details
+                                </button>
 
-                                <form action="{{ route('approvals.update', $approval) }}" method="post" class="my-form">
+                                {{--<form action="{{ route('approvals.update', $approval) }}" method="post" class="my-form">
                                     @csrf
                                     @method('PUT')
 
@@ -70,7 +70,7 @@
                                     >
                                         <i data-lucide="thumbs-down" class="w-4 h-4 mr-1"></i>
                                     </button>
-                                </form>
+                                </form>--}}
                             </div>
                         </td>
                     </tr>
@@ -85,90 +85,6 @@
         </div>
     </div>
     <!-- END: HTML Table Data -->
-
-    {{--<section class="mt-8 bg-white px-5 py-3">
-        <div class="sm:mb-3 flex flex-col-reverse sm:flex-row justify-between sm:items-center intro-y">
-            <p class="font-semibold">Showing list of all pending approvals for actions</p>
-        </div>
-        <div>
-            <div class="intro-y overflow-auto lg:overflow-visible">
-                <table class="table table-report table-auto table-hover sm:mt-2">
-                    <thead>
-                    <tr class="bg-gray-200">
-                        <th scope="col">#</th>
-
-                        <th scope="col">Resource</th>
-
-                        <th scope="col" class="text-center">Action</th>
-
-                        <th scope="col">Performed By</th>
-
-                        <th scope="col" class="text-center">Date</th>
-
-                        <th class="text-center whitespace-nowrap">
-                                <span class="flex justify-center">
-                                    <i data-lucide="settings" class="w-5 h-5"></i>
-                                </span>
-                        </th>
-                    </tr>
-                    </thead>
-
-                    <tbody>
-                    @foreach ($approvals as $approval)
-                        <tr class="intro-x">
-                            <td class="w-56">{{ $loop->iteration }}</td>
-
-                            <td class="w-56">{{ $approval->resource }}</td>
-
-                            <td class=""><x-badge :value="$approval->action" /></td>
-
-                            <td class="whitespace-nowrap">{{ $approval->author->name }}</td>
-
-                            <td class="whitespace-nowrap"><span class="text-dark">{{ $approval->created_at->toDayDateTimeString() }}</span></td>
-
-                            <td class="table-report__action w-48">
-                                <div class="flex justify-around items-center">
-                                    <button data-tw-toggle="modal" data-tw-target="#view-changes"
-                                       title="View changes"
-                                       class="flex items-center text-info cursor-pointer tooltip"
-                                    >
-                                        <i data-lucide="eye" class="w-4 h-4 mr-1"></i>
-                                    </button>
-
-                                    <form action="{{ route('approvals.update', $approval) }}" method="post" class="my-form">
-                                        @csrf
-                                        @method('PUT')
-
-                                        <button type="submit" title="Approve" @click="approval = @js($approval)"
-                                                class="flex items-center text-success cursor-pointer tooltip spinner-dark"
-
-                                        >
-                                            <i data-lucide="thumbs-up" class="w-4 h-4 mr-1"></i>
-                                        </button>
-                                    </form>
-
-                                    <form action="{{ route('approvals.destroy', $approval) }}" method="post" class="my-form">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" title="Decline"
-                                                class="flex items-center text-red-600 cursor-pointer tooltip spinner-dark"
-                                        >
-                                            <i data-lucide="thumbs-down" class="w-4 h-4 mr-1"></i>
-                                        </button>
-                                    </form>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-
-                <div>
-                    {{ $approvals->links() }}
-                </div>
-            </div>
-        </div>
-    </section>--}}
 
     <x-approvals.show-details />
 
